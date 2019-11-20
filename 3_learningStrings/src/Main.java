b2fa522548b42c36eefc98996808d88345f5999b
github username <github email address>
2019-11-20 17:39:08 -0600

    showed some String class functions

--- 3_learningStrings/src/Main.java
+++ 3_learningStrings/src/Main.java
@@ -1,2 +1,14 @@
 public class Main {
+
+    public static void main(String[] args) {
+        String userInput = "entertainment";
+
+        String upperCased = userInput.toUpperCase();
+        System.out.println(upperCased);
+
+        char firstCharacter = userInput.charAt(0);
+        System.out.println(firstCharacter);
+
+        System.out.println(userInput.contains("enter"));
+    }
 }
